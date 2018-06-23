4a0312090036f6fa39030b013a570a013c375843
Eric Riese <eric.riese@gmail.com>
2018-06-23 00:13:22 -0400

    why did I hard code the board size there?

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -9,8 +9,8 @@ class GameOfLife {
    }
 
    void set(Setter setter) {
-      for (int i = 0; i < 3; i++) {
-         for (int j = 0; j < 3; j++) {
+      for (int i = 0; i < board.length; i++) {
+         for (int j = 0; j < board[i].length; j++) {
             board[i][j] = setter.set(i, j, board[i][j]);
          }
       }
